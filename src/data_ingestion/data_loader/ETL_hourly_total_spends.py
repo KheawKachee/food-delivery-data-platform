6e02a8cdd61680a3f88252c2d5c6843c06f21023
KheawKachee <wachirawit.p.work@gmail.com>
2025-12-21 18:41:38 +0700

    add : ETL marts

---
+++ src/data_ingestion/data_loader/ETL_hourly_total_spends.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import datetime as dt
+import os
+import traceback
+import sys
+import glob
+import json
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+
+from airflow.utils.log.logging_mixin import LoggingMixin
+
+log = LoggingMixin().log
+
+
+def ETL_hourly_total_spends():
+    try:
+        pass
+    except Exception as e:
+        log.error("".join(traceback.format_exception(type(e), e, e.__traceback__)))
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    ETL_hourly_total_spends()

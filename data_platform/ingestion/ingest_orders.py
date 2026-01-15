b9b14bd7a3fa9e3abe9dc09835b6f53ee95c3a04
KheawKachee <wachirawit.p.work@gmail.com>
2026-01-16 04:09:22 +0700

    fix : JSONEncoder

--- data_platform/ingestion/ingest_orders.py
+++ data_platform/ingestion/ingest_orders.py
@@ -1,16 +1,24 @@
 import json
 import os
-from pathlib import Path
 from datetime import datetime, timezone
+from pathlib import Path
+
+import numpy as np
 import pandas as pd
-import numpy as np  # Add this for NaN handling
-
-from sqlalchemy import create_engine
-from sqlalchemy.dialects.postgresql import JSONB
-
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 load_dotenv()
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, pd.Timestamp):
+            return obj.isoformat()
+        if pd.isna(obj):
+            return None
+        return super().default(obj)
+
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 BASE_DIR = Path(__file__).resolve().parents[2]
@@ -25,17 +33,18 @@ df = pd.concat(
     ignore_index=True,
 )
 
-df['order_ts'] = pd.to_datetime(df['order_ts'])
-df = df.sort_values(by=['order_ts'],ascending=False).drop_duplicates(subset='order_id',keep='first')
+df["order_ts"] = pd.to_datetime(df["order_ts"])
+df = df.sort_values(by=["order_ts"], ascending=False).drop_duplicates(
+    subset="order_id", keep="first"
+)
 
 REQUIRED_COLS = {"order_id", "order_ts"}
 missing = REQUIRED_COLS - set(df.columns)
 if missing:
     raise ValueError(f"Missing required columns: {missing}")
 
-
 df = df.replace({np.nan: None})
-df["payload"] = df.apply(lambda r: json.dumps(r.to_dict()), axis=1)
+df["payload"] = df.apply(lambda r: json.dumps(r.to_dict(), cls=JSONEncoder), axis=1)
 
 
 ingest_ts = datetime.now(timezone.utc)
